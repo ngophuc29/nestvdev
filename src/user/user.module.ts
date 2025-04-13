efd316ff1730273fc905d00a12b02ffd27e6a135
ngophuc29 <ngophuc2911@gmail.com>
2025-04-13 15:32:19 +0700

    #12 làm về middleware

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entities';
-
+import { LoggerMiddleware } from  '../middleware/logger.middleware';
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [UserService],
   controllers: [UserController]
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes('*');
+  }
+}

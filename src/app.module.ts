42481919ec5e3667d6d8392e4be2903312e9fc8e
ngophuc29 <ngophuc2911@gmail.com>
2025-04-13 11:35:01 +0700

    #6 cách tích hợp orm và tạo table đầu tiên

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { TestService } from './test.service';
 import { TestController } from './test.controller';
 import { UserModule } from './user/user.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entities';
 @Module({
-  imports: [UserModule],
+  imports: [
+    
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'sapassword',
+      database: 'postnest',
+      entities: [User],
+      synchronize: true,
+    })
+    , UserModule],
   controllers: [AppController,TestController],
   providers: [AppService,TestService],
 })

fc75d50d2a1fd79ba9c306fea5fa278f4833d6f6
ngophuc29 <ngophuc2911@gmail.com>
2025-04-13 11:14:59 +0700

    #5 cách tạo 1 cái module (ở đây là User) bằng lệnh trong note

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestService } from './test.service';
 import { TestController } from './test.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController,TestController],
   providers: [AppService,TestService],
 })

426ea0b880fc7d58ec1c623e16f17dd6b06a2a74
ngophuc29 <ngophuc2911@gmail.com>
2025-04-13 14:25:34 +0700

    #8 hoan thien crud

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
 
+    constructor(private userService:UserService) {}
 
+  
+
+    @Post()
+    createUser(@Body() body: any) { 
+        return this.userService.createUser(body);
+    }
 
     @Get()
-    getUser() {
-        return "Hello User!";
+    findAll() {
+        return this.userService.findAll();
     }
+
+
+    @Get('/:id')
+    getUserById(@Param('id') id: number) { 
+        return this.userService.findById(id);
+    }
+
+    @Put('/:id')
+    updateUser(@Param('id') id: number, @Body() body: any) { 
+        return this.userService.updateById(id, body);
+    }
+
+    @Delete('/:id')
+    deleteUser(@Param('id') id: number ) {
+        return this.userService.deleteById(id );
+    }
+
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,64 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entities';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(@InjectRepository(User) private userRepo: Repository<User>) { }
+
+
+    async createUser(body: any) {
+        const { email, password } = body
+
+        const user = this.userRepo.create({ email, password });
+
+        return await this.userRepo.save(user);;
+    }
+
+    async findAll() {
+
+
+
+
+        return await this.userRepo.find({});
+    }
+
+
+    async findById(id: number) {
+
+
+
+
+        return await this.userRepo.findOneBy({ id });
+    }
+
+    async updateById(id: number, body: any) {
+
+
+        let user = await this.userRepo.findOneBy({ id });
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        Object.assign(user, body);
+
+        return this.userRepo.save(user) 
+    }
+
+    async deleteById(id: number ) {
+
+
+        let user = await this.userRepo.findOneBy({ id });
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+         
+
+        // return await this.userRepo.delete({ id })
+        return await this.userRepo.remove(user)
+
+    }
+}
